c52cae3ad6f6b82c0392e01a0615b06071b08c19
João Vitor Miranda <joao.vitor.miranda.siqueira@gmail.com>
2025-03-24 13:30:14 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
