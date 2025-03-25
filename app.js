ed53925fd620b6e0871a70fcd1ca7595644dd64f
João Vitor Miranda <joao.vitor.miranda.siqueira@gmail.com>
2025-03-25 10:10:03 -0400

    Initial structure

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('');
+  res.status(500).send('Internal server error.');
 });
 
 const PORT = process.env.PORT || 3000;
